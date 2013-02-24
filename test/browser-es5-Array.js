1919b9bed5812e92ce93a17167086060a5def059
Marcello Bastea-Forte <marcello@cellosoft.com>
2013-02-24 08:35:37 -0500

    more tests

--- test/browser-es5-Array.js
+++ test/browser-es5-Array.js
@@ -2,6 +2,23 @@ var assert = require('assert')
 
 suite('es5-Array')
 
+var EMPTY_ARRAY = []
+var ARRAY_123 = [1,2,3]
+var ARRAY_12345 = [1,2,3,4,5]
+var NEW_ARRAY = new Array(5)
+
+var SPARSE_ARRAY = []
+SPARSE_ARRAY[0] = 'foo'
+SPARSE_ARRAY[100] = 'bar'
+
+var SPARSE_ARRAY2 = [1,2,3]
+SPARSE_ARRAY2[9] = 10
+
+var SPARSE_ARRAY3 = []
+SPARSE_ARRAY3[-5] = 'uh oh'
+SPARSE_ARRAY3[0] = 'ok'
+SPARSE_ARRAY3[10000] = 'seriously?'
+
 test('Array.isArray', function() {
 
     require('../es5/Array.isArray.js')
@@ -10,6 +27,13 @@ test('Array.isArray', function() {
     assert.equal(true, Array.isArray([1,2,3]))
     assert.equal(true, Array.isArray([]))
     assert.equal(true, Array.isArray(new Array(5)))
+    assert.equal(true, Array.isArray(EMPTY_ARRAY))
+    assert.equal(true, Array.isArray(ARRAY_123))
+    assert.equal(true, Array.isArray(ARRAY_12345))
+    assert.equal(true, Array.isArray(NEW_ARRAY))
+    assert.equal(true, Array.isArray(SPARSE_ARRAY))
+    assert.equal(true, Array.isArray(SPARSE_ARRAY2))
+    assert.equal(true, Array.isArray(SPARSE_ARRAY3))
 
     assert.equal(false, Array.isArray(45))
     assert.equal(false, Array.isArray(undefined))
@@ -22,6 +46,22 @@ test('Array.prototype.every', function () {
     require('../es5/Array.prototype.every.js')
 
     assert.ok(Array.prototype.every)
+    
+    var count = 0
+    ARRAY_123.every(function(x) { count++; return x < 2 })
+    assert.equal(2, count)
+    
+    count = 0
+    ARRAY_123.every(function() { count++; return false })
+    assert.equal(1, count)
+    
+    count = 0
+    ARRAY_12345.every(function() { count++; return true })
+    assert.equal(5, count)
+    
+    count = 0
+    EMPTY_ARRAY.every(function() { count++; return true })
+    assert.equal(0, count)
 })
 
 test('Array.prototype.filter', function () {
